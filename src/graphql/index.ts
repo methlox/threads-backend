127bfa0fa9a886d5f4783fd9c8ca610da23936af
Sid <astroyellowh@gmail.com>
2023-09-11 13:21:20 +0530

    first mutation

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,18 +1,25 @@
 import { ApolloServer } from "@apollo/server";
+import { User } from "./user";
 
 async function createApolloGqlServer() {
   // create gql server
   const gqlServer = new ApolloServer({
     typeDefs: `
     type Query {
+        ${User.queries}
     }
     type Mutation {
+        ${User.mutations}
     }
     `, // schemas
     resolvers: {
-      Query: {},
-      Mutation: {}
-        },
+      Query: {
+        ...User.resolvers.queries,
+      },
+      Mutation: {
+        ...User.resolvers.mutations,
+      },
+    },
   });
 
   await gqlServer.start();
