f862ea7b156d3f619cacda548496edd1d6399b7b
Sid <astroyellowh@gmail.com>
2023-09-11 11:50:55 +0530

    added a mutatation query

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
+import { prismaClient } from "./lib/db";
 
 async function init() {
   const app = express();
@@ -16,12 +17,42 @@ async function init() {
         hello: String
         say(name: String): String
     }
+    type Mutation {
+      createUser(firstName: String!, lastName: String!, email: String!, password: String!): Boolean
+    }
     `, // schemas
     resolvers: {
-        Query:{
-            hello: () => `whats poppin`,
-            say: (_, {name}: {name: String} ) => `Hey ${name}, whats going on?`
+      Query: {
+        hello: () => `whats poppin`,
+        say: (_, { name }: { name: String }) => `Hey ${name}, whats going on?`,
+      },
+      Mutation: {
+        createUser: async (
+          _,
+          {
+            firstName,
+            lastName,
+            email,
+            password,
+          }: {
+            firstName: string;
+            lastName: string;
+            email: string;
+            password: string;
+          }
+        ) => {
+          await prismaClient.user.create({
+            data: {
+              email,
+              firstName,
+              lastName,
+              password,
+              salt: "random_salt",
+            },
+          });
+          return true;
         },
+      },
     }, // functions which will execute
   });
 
@@ -37,4 +68,4 @@ async function init() {
   app.listen(PORT, () => console.log(`Server started at PORT: ${PORT}`));
 }
 
-init();  
+init();
